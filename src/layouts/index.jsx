31a06cf240e91aaeac909c2a5c14b1647d0948da
ABS <antok.benedek@gmail.com>
2018-04-04 01:24:55 +0200

    add favicon

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -3,10 +3,13 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
 import '../layouts/index.css';
+import favicon from '../data/favicon.ico';
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Új lap" />
+    <Helmet title='Új lap'>
+      <link rel="shortcut icon" href={favicon} />
+    </Helmet>
     {children()}
   </div>
 )
