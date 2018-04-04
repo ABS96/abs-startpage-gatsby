7ee83a55fec53fc68f5ac754297fb8fe7315beb1
ABS <antok.benedek@gmail.com>
2018-04-04 04:01:10 +0200

    change backticks to single quotes in gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,12 +3,12 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        path: `./src/data/`,
+        path: './src/data/',
       },
     },
-    `gatsby-transformer-yaml`,
-    `gatsby-plugin-emotion`,
+    'gatsby-transformer-yaml',
+    'gatsby-plugin-emotion',
   ],
 }
